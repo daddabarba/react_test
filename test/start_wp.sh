365bdb8125ec89968ca1f4d09457c8c2fcbda3ac
daddabarba <davidebarbieri97@gmail.com>
2018-05-22 18:34:27 +0200

    changed launch script relative path

--- test/start_wp.sh
+++ test/start_wp.sh
@@ -1,5 +1,5 @@
 echo "starting"
 cd src
-gnome-terminal -x bash -c "mongod --dbpath /home/daddabarba/Desktop/testApp/test/data/ --port 27018;"
+gnome-terminal -x bash -c "mongod --dbpath ../data/ --port 27018;"
 gnome-terminal -x bash -c "node server.js;"
 npm start
