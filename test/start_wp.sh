8b3a84bced39d821430151656ee1ca76b0daa328
daddabarba <davidebarbieri97@gmail.com>
2018-06-27 01:01:01 +0200

    changes launch script

--- test/start_wp.sh
+++ test/start_wp.sh
@@ -2,4 +2,4 @@ echo "starting"
 cd src
 gnome-terminal -x bash -c "mongod --dbpath ../data/ --port 27018;"
 gnome-terminal -x bash -c "sleep 5s;node server.js;"
-npm start
+gnome-terminal -x bash -c "npm start;"
